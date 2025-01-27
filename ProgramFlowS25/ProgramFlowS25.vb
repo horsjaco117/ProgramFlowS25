398ecef41a08ab5ffcf2c612abce272b9265abe7
horsjaco117 <jacob_horsley@live.com>
2025-01-27 08:52:27 -0700

    Cases started but not finished

--- ProgramFlowS25/ProgramFlowS25.vb
+++ ProgramFlowS25/ProgramFlowS25.vb
@@ -11,16 +11,17 @@ Module ProgramFlowS25
 
 
     Sub Main()
-        Dim funnynumber As Integer
+        Dim funnyNumber As Integer
+        Dim userInput As String
 
         'Assign number to 5
-        funnynumber = 5
+        funnyNumber = 5
 
         'test is the number greater than 6
         'display the result on the console
         '(doesn't display because it results in a false)
-        If funnynumber > 6 Then
-            Console.WriteLine(funnynumber > 6)
+        If funnyNumber > 6 Then
+            Console.WriteLine(funnyNumber > 6)
         End If
 
 
@@ -33,6 +34,7 @@ Module ProgramFlowS25
         End If
 
 
+        funnynumber -= 3
 
         'Due to the else if statement it become an or gate essentially
         If funnynumber > 6 Then
@@ -46,7 +48,37 @@ Module ProgramFlowS25
             Console.WriteLine(funnynumber > 4)
         End If
 
+        'If nothing evaluates true the else will be selected
+        If funnyNumber > 6 Then
+            Console.WriteLine($"{funnyNumber} is a big number!")
 
+        ElseIf funnyNumber > 3 Then
+            Console.WriteLine($"Sorry not tall enough...")
+        ElseIf funnyNumber > 4 Then
+            Console.WriteLine($"Who cares")
+        Else
+            Console.WriteLine($"Not sure what happened")
+        End If
+
+        'If one is true it skips the rest
+        Console.WriteLine("Please enter your age:")
+        userInput = Console.ReadLine()
+        funnyNumber = CInt(userInput)
+
+        Select Case funnyNumber
+            Case 0 To 3
+
+            Case 4 To 10
+
+            Case 11 To 64
+
+            Case 65 To 99
+
+            Case > 100
+
+            Case Else
+
+        End Select
     End Sub
 
 End Module
